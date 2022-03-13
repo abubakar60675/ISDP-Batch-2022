ba5c17060cc5f58e8a33106d9d2cdfd5ee9ae725
Muhammad Abubakar <abubakar60675@gmail.com>
2022-03-13 21:56:56 +0500

    Assignment - Convert this express server to Mongodb

--- 15-express-crud/app.js
+++ 15-express-crud/app.js
@@ -5,47 +5,47 @@ let products = ["Shampoo", "Dove"];
 app.use(express.json());
 
 //default
-app.get("/", function (req, res) {
-  res.send("Hello World I am here");
+app.get("/", function(req, res) {
+    res.send("Hello World I am here");
 });
 
 //get all
-app.get("/api/products", function (req, res) {
-  res.send(products);
+app.get("/api/products", function(req, res) {
+    res.send(products);
 });
 //getOne
-app.get("/api/products/:id", function (req, res) {
-  res.send(products[req.params.id]);
+app.get("/api/products/:id", function(req, res) {
+    res.send(products[req.params.id]);
 });
 
 //Post data
-app.post("/api/products", function (req, res) {
-  var data = req.body;
-  products.push(data.name);
-  res.send(data.name);
-  //   res.send("Product created");
+app.post("/api/products", function(req, res) {
+    var data = req.body;
+    products.push(data.name);
+    res.send(data.name);
+    //   res.send("Product created");
 });
 //Put data
-app.put("/api/products/:id", function (req, res) {
-  var data = req.body;
-  console.log(data);
-  products[req.params.id] = data.name;
-  res.send(data.name);
-  // var productToEdit = products[req.params.id];
+app.put("/api/products/:id", function(req, res) {
+    var data = req.body;
+    console.log(data);
+    products[req.params.id] = data.name;
+    res.send(data.name);
+    // var productToEdit = products[req.params.id];
 
-  // productToEdit = data.name;
+    // productToEdit = data.name;
 
-  // products[req.params.id] = productToEdit;
-  // res.send(productToEdit);
-  //   res.send("Product created");
+    // products[req.params.id] = productToEdit;
+    // res.send(productToEdit);
+    //   res.send("Product created");
 });
 //Delete data
-app.delete("/api/products/:id", function (req, res) {
-  var product = products[req.params.id];
-  products.splice(req.params.id, 1);
+app.delete("/api/products/:id", function(req, res) {
+    var product = products[req.params.id];
+    products.splice(req.params.id, 1);
 
-  res.send(product);
-  //   res.send("Product created");
+    res.send(product);
+    //   res.send("Product created");
 });
 
-app.listen(8080);
+app.listen(8081);
